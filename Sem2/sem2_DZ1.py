af536da7faec86b4a39bd188eb3d389c1639d30d
globa78 <pavlglob@gmail.com>
2022-11-14 01:32:31 +0300

    домашнее задание

--- Sem2/sem2_DZ1.py
+++ Sem2/sem2_DZ1.py
@@ -3,17 +3,6 @@
 # Пример:
 # 
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = int(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Число должно быть integer ")
-    return number
-
-
 def mult(n):
     if n == 1:
         return 1
@@ -21,7 +10,7 @@ def mult(n):
         return n * mult(n - 1)
 
 
-num = InputNumbers("Введите число: ")
+num = int(input("Введите число: "))
 
 list = []
 for e in range(1, num + 1):
